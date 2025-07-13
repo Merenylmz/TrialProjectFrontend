08d3ad083e6982c6f0cd8fc10410b75348be3f61
Muhammet Eren <115183696+Merenylmz@users.noreply.github.com>
2025-07-13 14:30:17 +0300

    MinimalFixingForCrash

--- src/utils/CommonAPI.ts
+++ src/utils/CommonAPI.ts
@@ -1,30 +1,33 @@
 import axios from "axios";
 
 const CommonAPI = async({url, method, parameters, inputs} : {url: string, method: string, parameters: string, inputs?: object}) => {
-    
-    let data : object = {};
-    const text : string = `${url}/${(parameters && parameters)}`;
-    switch (method) {
-        case "GET":
-            const gres = await axios.get(text);
-            data = gres.data;
-            break;
-        case "POST":
-            const pres = await axios.post(text, inputs);
-            data = pres.data;
-            break;
-        case "DELETE":
-            const dres = await axios.delete(text);
-            data = dres.data;
-            break;
-        case "EDIT":
-            const eres = await axios.post(text, inputs);
-            data = eres.data;
-            break;
-        default:
-            return data;
+    try {
+        let data : object = {};
+        const text : string = `${url}/${(parameters && parameters)}`;
+        switch (method) {
+            case "GET":
+                const gres = await axios.get(text);
+                data = gres.data;
+                break;
+            case "POST":
+                const pres = await axios.post(text, inputs);
+                data = pres.data;
+                break;
+            case "DELETE":
+                const dres = await axios.delete(text);
+                data = dres.data;
+                break;
+            case "EDIT":
+                const eres = await axios.post(text, inputs);
+                data = eres.data;
+                break;
+            default:
+                return data;
+        }
+        return data;
+    } catch (error) {
+        console.log(error);
     }
-    return data;
 }
 
 export default CommonAPI;
